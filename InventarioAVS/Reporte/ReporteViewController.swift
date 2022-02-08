666d0511deb8397b5e07ed8534d246f647106298
omar campos <ruben.campos@sportsworld.com.mx>
2022-02-08 11:47:33 -0600

    add report storyboard

---
+++ InventarioAVS/Reporte/ReporteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReporteViewController.swift
+//  InventarioAVS
+//
+//  Created by Ruben  Omar Campos Vazquez on 05/02/22.
+//
+
+import UIKit
+
+class ReporteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
